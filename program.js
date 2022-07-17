604ee7d9d57f03fa351d89d4169ef0cbd36f30ac
David Bilson <85068757+dlbi@users.noreply.github.com>
2022-07-18 00:58:33 +0100

    Add files via upload

--- program.js
+++ program.js
@@ -1,13 +1,34 @@
-// Notice Modal
+const lrnMre = document.getElementById('lrnPop');
+const lrnBx = document.querySelector('#learnMore');
+const clsLrn = document.querySelector('#cls');
+lrnMre.addEventListener('click', function() {
+    lrnBx.style.display = 'block';
+});
+clsLrn.addEventListener('click', function() {
+    lrnBx.style.display = 'none';
+});
+// Sticky navbar
+window.onscroll = function() {
+    myFunction();
+};
+let navBar = document.getElementById('navBar');
+let sticky = navBar.offsetTop;
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navBar.classList.add('sticky');
+    } else {
+        navBar.classList.remove('sticky');
+    }
+}
+// // Notice Modal
 const noticeModal = document.querySelector('#myNoticeModal');
 let closeNotice = document.querySelector('#close');
 let closeNoticeBtn = document.querySelector('#close-pop');
-
 setTimeout(
     function(){
         noticeModal.style.display = 'block';
     }
-    , 5000
+    , 7000
 );
 window.onclick = function(event) {
     if (event.target === noticeModal) {
@@ -19,7 +40,7 @@ closeNotice.addEventListener('click', function(){
 });
 closeNoticeBtn.addEventListener('click', function(){
     noticeModal.style.display = 'none';
-})
+});
 // MODAL BEGINNING
 const modal = document.querySelector('#myModal');
 const ibtn = document.querySelector('#openIModal');
@@ -29,7 +50,6 @@ const cpbtn = document.querySelector('#openCPModal');
 let span = document.getElementsByClassName('close');
 let formName = document.querySelector('.exam-name');
 let examType = ['IELTS', 'TOEFL', 'CAE', 'CPE'];
-
 ibtn.onclick = function() {
     modal.style.display = 'block';
     formName.textContent = examType[0];
@@ -39,13 +59,11 @@ span.onclick = function() {
     modal.style.display = 'none';
     noticeModal.style.display = 'none';
 };
-
 window.onclick = function(event) {
     if (event.target === modal) {
         modal.style.display = 'none';
     }
 };
-
 tbtn.onclick = function() {
     modal.style.display = 'block';
     formName.textContent = examType[1];
@@ -56,14 +74,12 @@ cabtn.onclick = function() {
     formName.textContent = examType[2];
     formName.style.fontSize = '1rem';
 };
-
 cpbtn.onclick = function() {
     modal.style.display = 'block';
     formName.textContent = examType[3];
     formName.style.fontSize = '1rem';
 };
 // MODAL ENDING
-
 // set conditionals, if form is empty, display error else if form is not empty, 'next' button should appear...
 // form validation ()
 const fname = document.forms['myForm']['First_Name'].value;
@@ -80,5 +96,4 @@ startBtn.addEventListener('click', function(){
     nextBtn.classList.add('next-style');
 });
 nextBtn.addEventListener('click', function(){
-    
 })
